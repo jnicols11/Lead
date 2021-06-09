78d679ed8529dd99171170da00e42585a0ec79ca
Jordan Nicols <nicolsj99@gmail.com>
2021-06-08 20:33:56 -0700

    finished manage project users feature

--- src/app/project/project-dashboard/project-dashboard-manage-users/project-dashboard-manage-users.component.ts
+++ src/app/project/project-dashboard/project-dashboard-manage-users/project-dashboard-manage-users.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { UserHttpService } from 'src/app/user/user-http.service';
 import { User } from 'src/app/user/user.model';
 import { ProjectHttpService } from '../../project-http.service';
+import { Project } from '../../project.model';
 
 @Component({
   selector: 'app-project-dashboard-manage-users',
@@ -13,6 +14,7 @@ import { ProjectHttpService } from '../../project-http.service';
 export class ProjectDashboardManageUsersComponent implements OnInit {
   users: User[] = [];
   focusedUser: User = null;
+  projectID: string;
   loading = true;
   loadFailed = false;
   error = new Subject<string>();
@@ -28,38 +30,8 @@ export class ProjectDashboardManageUsersComponent implements OnInit {
     this.route.queryParams
       .subscribe(
         params => {
-          this.projectService.getProjectById(params['id'])
-            .subscribe(
-              projectData => {
-                const users = projectData.body['users'];
-                users.forEach(id => {
-                  this.userService.getUser(id.id)
-                    .subscribe(
-                      userData => {
-                        // populate user model
-                        let user = new User(
-                          userData.body['fullName'],
-                          userData.body['username'],
-                          userData.body['email'],
-                          null,
-                          userData.body['ID'],
-                          id.role
-                        );
-
-                        // push user to component user array
-                        this.users.push(user);
-                        this.loading = false;
-                      }, error => {
-                        this.error.next(error.message);
-                      }
-                    )
-                });
-              }, error => {
-                this.loading = false;
-                this.loadFailed = true;
-                this.error.next(error.message);
-              }
-            )
+          this.projectID = params['id'];
+          this.populateUsers();
         }
       )
   }
@@ -72,7 +44,133 @@ export class ProjectDashboardManageUsersComponent implements OnInit {
     this.focusedUser = null;
   }
 
-  removeUserFromProject() {
+  updateUserRole(role: string) {
+    // update user role in database
+    this.users.forEach(user => {
+      if(user.id == this.focusedUser.id) {
+        user.role = role;
+      }
+    });
 
+    this.updateProject();
+
+  }
+
+  removeUserFromProject() {
+    this.loading = true;
+    this.projectService.getProjectById(this.projectID)
+      .subscribe(
+        projectData => {
+          // get list of users
+          const users = projectData.body['users'];
+
+          // remove user from list
+          users.forEach((element, index) => {
+            if(element.id == this.focusedUser.id) users.splice(index,1);
+          });
+
+          //update list in database
+          this.updateProjectUsers(users);
+
+          // reset focus
+          this.focusedUser = null;
+        }, error => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  private populateUsers() {
+    this.users = [];
+    this.projectService.getProjectById(this.projectID)
+      .subscribe(
+        projectData => {
+          const users = projectData.body['users'];
+          users.forEach(id => {
+            this.userService.getUser(id.id)
+              .subscribe(
+                userData => {
+                  // populate user model
+                  let user = new User(
+                    userData.body['fullName'],
+                    userData.body['username'],
+                    userData.body['email'],
+                    null,
+                    userData.body['ID'],
+                    id.role
+                  );
+
+                  // push user to component user array
+                  this.users.push(user);
+                  this.loading = false;
+                }, error => {
+                  this.error.next(error.message);
+                }
+              )
+          });
+        }, error => {
+          this.loading = false;
+          this.loadFailed = true;
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  private updateProject() {
+    const project: Project = new Project;
+
+    this.projectService.getProjectById(this.projectID)
+      .subscribe(
+        projectData => {
+          project.name = projectData.body['name'];
+          project.desc = projectData.body['desc'];
+          project.deadline = projectData.body['deadline'];
+          project.userID = projectData.body['userID'];
+          project.id = projectData.body['_id'];
+
+          project.users = this.users;
+
+          this.projectService.updateProject(project)
+            .subscribe(
+              responseData => {
+                // success!!
+              }, error => {
+                this.error.next(error.message);
+              }
+            );
+        }, error => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  private updateProjectUsers(users: { id: number, role: string }[]) {
+    const project: Project = new Project;
+
+    this.projectService.getProjectById(this.projectID)
+      .subscribe(
+        projectData => {
+          project.name = projectData.body['name'];
+          project.desc = projectData.body['desc'];
+          project.deadline = projectData.body['deadline'];
+          project.userID = projectData.body['userID'];
+          project.id = projectData.body['_id'];
+
+          project.users = users;
+
+          this.projectService.updateProject(project)
+            .subscribe(
+              responseData => {
+
+                // update list on screen
+                this.populateUsers();
+              }, error => {
+                this.error.next(error.message);
+              }
+            );
+        }, error => {
+          this.error.next(error.message);
+        }
+      );
   }
 }
